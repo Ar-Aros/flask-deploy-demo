b6886bcbc66bb559088826f347d91bb0ec57b0e3
Md. Abdur Rahim Sojib <sojib@bracnet.com>
2025-10-13 22:32:49 +0600

    test

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, World! 🚀 Deployed with Docker + GitHub Actions + AWS EC2"
+@app.route("/test")
+def test():
+    return "Workflow is working! ✅"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050)
+    app.run(host='0.0.0.0', port=5000)
